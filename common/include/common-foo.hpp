e88e546f979aaffa46745cfc50567444c2ae108f
Michal Kukowski <kukossw@gmail.com>
2024-08-10 18:39:48 +0200

    Add common library as a shared common
    The project already contains 4 binaries.
    HOST device:
    - deamon.out
    - health-monitor.out
    
    IoT:
    - data-provider-battery.out
    - data-provider-disk.out
    
    Sometimes the code will be the same with different config
    or different parameters passed to the function / class / method.
    To achieve this the common directory has been introduced.
    
    Hovewer, sometimes the compile time option like defined (-D)
    will be needed (logger is a perfect example).
    To make it possible, each binary needs to recompile the common code.
    This commit has an example for such a situation.

---
+++ common/include/common-foo.hpp
@@ -0,0 +1,10 @@
+#ifndef COMMON_FOO_H
+#define COMMON_FOO_H
+
+#ifndef COMMON_TEST_DEFINE_1
+#define COMMON_TEST_DEFINE_1 10
+#endif // COMMON_TEST_DEFINE_1
+
+void hello();
+
+#endif // COMMON_FOO_H
--- data-provider-battery/app/main.cpp
+++ data-provider-battery/app/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include <common-foo.hpp>
+
 int main()
 {
     std::cout << "Data-Provider-Battery" << std::endl;
+
+    hello();
 
     return 0;
 }
