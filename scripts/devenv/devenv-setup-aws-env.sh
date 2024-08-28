077730b7d7df785fe23fb248e06a7b40059db21a
Norbert Kamiński <norbert.kaminski@infogain.com>
2024-08-28 12:17:01 +0200

    scripts: Add aws-sdk-cpp setup script
    This patch splits existing env setup script to speed up CI jobs.
    Also it adds new script to setup, build, and install aws-sdk-cpp.
    
    Signed-off-by: Norbert Kamiński <norbert.kaminski@infogain.com>

---
+++ scripts/devenv/devenv-setup-aws-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function install_aws_sdk()
+{
+    # Clone aws-sdk-cpp source
+    git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp
+
+    # Install dependencies
+    apt install -y libcurl4-openssl-dev libssl-dev uuid-dev zlib1g-dev libpulse-dev
+
+    # Build aws-sdk for C++ with S3 service
+    mkdir sdk_build && cd sdk_build
+    cmake ../aws-sdk-cpp -DCMAKE_BUILD_TYPE=Debug -DFORCE_SHARED_CRT=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_PREFIX_PATH=/usr/ -DCMAKE_INSTALL_PREFIX=/usr/ -DBUILD_ONLY="s3"
+    make
+
+    # Install aws-sdk for C++
+    make install
+}
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt update
+apt upgrade -y
+
+install_aws_sdk
